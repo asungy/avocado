455f0e012e3a972101fe08408627fd052141daaf
Alek Sung <aleksungis@gmail.com>
2020-11-21 10:42:22 -0500

    Removing versioning

--- source/main.cpp
+++ source/main.cpp
@@ -1,16 +1,9 @@
-
-#include "ProjectVars.hpp"
 #include "python.hpp"
 
 #include <iostream>
 
 int main( int argc, char * argv[] )
 {
-    std::cout << "Hello, Avocado!" << std::endl;
-
-    std::cout << argv[0] << " Version: " << PROJECT_VERSION_MAJOR << "."
-              << PROJECT_VERSION_MINOR << "." << PROJECT_VERSION_PATCH << std::endl;
-
     Python::run(argv[0]);
 
     return 0;
--- vars/README.md
+++
@@ -1,1 +0,0 @@
-Contains project configuration varibles to be used in the build process
